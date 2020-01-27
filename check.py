136497a5562aa50201e80c5abede163e868d6200
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-01-27 01:08:48 -0500

    Update check.py (mostly beautify)
    - Remove 10,000 character limitation in password
    - Remove unnecessary brackets from if statements
    - Improve spacing around function calls, punctuation, control flow, etc.
    - Other spacing tweaks

--- check.py
+++ check.py
@@ -6,33 +6,32 @@ def consec(password):
 
 def check_password(password):
   meets_requirements = False
-  #min length is 12
-  #at least 1 digit
-  #at least 1 uppper and 1 lower char
-  #at least 1 special char
+  # min length is 12
+  # at least 1 digit
+  # at least 1 uppper and 1 lower char
+  # at least 1 special char
 
-  x = [i for i in password.split()]
+  x = [ i for i in password.split() ]
 
-  if(len(x)==1):
-    r_p1 = re.compile('^(?=\S{12,10000}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])')
+  if len(x) == 1:
+    r_p1 = re.compile('^(?=\S{12,}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])')
 
-    s = re.search(r_p1,password)
-    if (s and consec(password)):
-      meets_requirements=True
+    s = re.search(r_p1, password)
+    if s and consec(password):
+      meets_requirements = True
 
   else:
-    w_count=len(x)
-    if(w_count<5):
+    w_count = len(x)
+    if w_count < 5:
       return False
-    for i in range (w_count):
-      if(not(len(x[i]) > 2 and x[i].isalpha())):
+    for i in range(w_count):
+      if not (len(x[i]) > 2 and x[i].isalpha()):
         return False
-    meets_requirements=True
-  
+
+    meets_requirements = True
 
   return meets_requirements
 
-  
 
 password = input()
 print(check_password(password))
